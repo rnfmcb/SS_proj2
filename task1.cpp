f2626f828b638119c7520303a7e87f7ad1f54c20
Grayson C Hart <gchxr9@delmar.umsl.edu>
2018-12-09 13:05:39 -0600

    Added username generator

--- task1.cpp
+++ task1.cpp
@@ -16,6 +16,16 @@ struct user_t {
 	unsigned char hashedPass[32];
 }user;
 
+void gen_username(char *s, const int len){
+	static const alplhanum[] = 
+		"0123456789"
+		"abcdefghijklmnopqrstuvwxyz";
+	for(int i = 0; i < len; ++i){
+		s[i] = alphanum[rand()%(sizeof(alphanum)-1]
+	}
+	s[len] = 0;
+}
+
 void gen_random(char *s, const int len) {
     static const char alphanum[] =
         "0123456789"
@@ -25,7 +35,6 @@ void gen_random(char *s, const int len) {
     for (int i = 0; i < len; ++i) {
         s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
-
     s[len] = 0;
 }
 
